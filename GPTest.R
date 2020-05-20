dc1e7777749eb82cb2b8cf57e10698d33f7213a6
Ryan Schultz <ryanschultz2018@yahoo.com>
2020-05-20 11:56:05 -0400

    Refactored cornerPeak function. Calculates for entire input space.

--- GPTest.R
+++ GPTest.R
@@ -4,28 +4,15 @@ library(lhs)
 ## 1D Example 1
 source("C:/Users/ryans/OneDrive/Desktop/KSSS with Leatherman/Adaptive Partitioning Emulator/CornerPeakFunction.R")
 
-numInputs = 85
-dimensions = 2
+numInputs <- 85
+dimensions <- 2
 
 set.seed(seed = NULL)
-inputs = maximinLHS(numInputs, dimensions)
+inputs <- maximinLHS(numInputs, dimensions)
 inputs
-outputs <- cornerPeak(inputs[1,])
-outputs
-i <- 2
-repeat
-{
-  y <- cornerPeak(inputs[i,])
-  outputs <- c(outputs, y)
-  i <- i + 1
-  if(i > numInputs)
-  {
-    break
-  }
-}
-inputs
+outputs <- cornerPeak(inputs)
 outputs
 
-GPmodel <- GP_fit(inputs, t(outputs))
+GPmodel <- GP_fit(inputs, outputs)
 print(GPmodel)
 plot(GPmodel, resolution = 100, line_type = c(6,2), pch = 5)
