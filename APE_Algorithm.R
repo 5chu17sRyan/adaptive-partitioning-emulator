10a52a5e229da6b5bfd16558ffaa17bb8c3e903a
Ryan Schultz <ryanschultz2018@yahoo.com>
2020-05-27 12:38:16 -0400

    Coded algorithm "Find region w/ largest... add new responses to..."

--- APE_Algorithm.R
+++ APE_Algorithm.R
@@ -6,18 +6,14 @@ library(ramify)
 source("C:/Users/ryans/OneDrive/Desktop/KSSS with Leatherman/Adaptive Partitioning Emulator/CornerPeakFunction.R")
 source("C:/Users/ryans/OneDrive/Desktop/KSSS with Leatherman/Adaptive Partitioning Emulator/GP_LOOCV.R")
 
-
-numInputs <- 85
-dimensions <- 2
+maxDesignSize <- 200
+numInputs <- 20
+dimensions <- 2 # d
 
 set.seed(seed = NULL)
 x <- maximinLHS(numInputs, dimensions)
 
-initialRegion <- NULL
-for (i in x) 
-{
-  initialRegion <- c(initialRegion, 1)
-}
+initialRegion <- rep(1,numInputs)
 
 points <- data.frame(
   regions = initialRegion,
@@ -35,14 +31,6 @@ regions <- data.frame(
   rUpperBounds = matrix(initialUpperBounds, 1, dimensions)
 )
 
-#Subset points to be from region 1.
-regionOne <- regions[regions$regionID == 1,]
-
-#subsettedPoints = points
-#R1_UpperBound <- regionOne$rUpperBounds.1
-#subsettedPoints = points[points$inputs.1 < R1_UpperBound & points$inputs.2 <regionOne$rUpperBounds.2,]
-#subsettedPoints
-
 startInputIndex = 3
 endInputIndex = 2+dimensions
 outputIndex = 2
@@ -53,10 +41,43 @@ GPmodelR1 <- GP_fit(points[,startInputIndex:endInputIndex], points[,outputIndex]
 #Calculate the CV estimate of GP prediction error over R1
 errorR1 <- GP_LOOCV(points)
 regions['errors'] <- errorR1
-regions
+regions$errors
 
 #Set overall design size to be number of inputs
-designSize <- numInputs
+designSize <- numInputs # n0
 
 #Set number of regions to be 1
 numRegions <- 1
+
+while(designSize < maxDesignSize){
+  #Find the region with the largest prediction error
+  maxErrorRegionIndex <- which.max(regions$errors) # k*
+  maxErrorRegion <- regions[regions$regionID == maxErrorRegionIndex] # Rk*
+  maxErrorRegion
+  
+  #Find the number of points that currently lie in Rk*
+  pointsRk <- points[points$regions == maxErrorRegionIndex,]
+  numPointsRk <- nrow(pointsRk) # n*
+  
+  #Generate a new random (2n0 - n*) x d LHD within Rk
+  newInputs <- maximinLHS(2*designSize-numPointsRk, dimensions)
+  newInputs
+  #Scale to be within the lower an upper bounds of Rk
+  i <- 1
+  while(i < dimensions){
+    upperBound = maxErrorRegion[,dimensions+1+i]
+    lowerBound = maxErrorRegion[,1+i]
+    newInputs[,i] = newInputs[,i]*(upperBound-lowerBound) + lowerBound
+    i = i + 1
+  }
+  
+  newPointsRegion <- rep(maxErrorRegionIndex,numPointsRk)
+  
+  #Evaluate the new responses at this new LHD : y(new)
+  newOutputs <- cornerPeak(newInputs)
+  
+  #Add the new points to the overall design : X <-{X, x(new)}
+  newPoints <- data.frame(regions = newPointsRegion, outputs = newOutputs, inputs = newInputs)
+  points <- rbind(points, newPoints)
+  
+}
